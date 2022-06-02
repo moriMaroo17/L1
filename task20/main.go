55b564544d76e057983fca794d9c086a15d1cf09
moriMaroo17 <morimaroo17@gmail.com>
2022-06-02 15:11:29 +0300

    task 20 completed

---
+++ task20/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func reverse(value string) {
+	// Convert to rune array
+	splitedValue := strings.Fields(value)
+	// Create builder
+	builder := strings.Builder{}
+	// Iterate from end to start of words array
+	for i := len(splitedValue) - 1; i >= 0; i-- {
+		builder.WriteString(fmt.Sprintf("%s ", splitedValue[i]))
+	}
+	fmt.Println(strings.TrimSpace(builder.String()))
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	// Endlessly scan stdin
+	for scanner.Scan() {
+		reverse(scanner.Text())
+	}
+}
