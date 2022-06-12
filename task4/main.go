071dd97fd9a14dfa6b46ce751dbaf27cce42d58e
moriMaroo17 <morimaroo17@gmail.com>
2022-06-12 16:47:17 +0300

    modified task4

--- task4/main.go
+++ task4/main.go
@@ -7,16 +7,14 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"sync"
 	"time"
 )
 
 // Worker function template. It sending date from channel to stdout and dies
 // with closing channel
-func worker(wg *sync.WaitGroup, readChannel <-chan any, number int) {
+func worker(readChannel <-chan any, number int) {
 	// Notify wait group about done work, and send message about death to stdout
 	defer io.WriteString(os.Stdout, fmt.Sprintf("worker number %d died\n", number))
-	defer wg.Done()
 	// Endlessly listening readChannel while it's open
 	for value := range readChannel {
 		io.WriteString(
@@ -28,7 +26,6 @@ func worker(wg *sync.WaitGroup, readChannel <-chan any, number int) {
 }
 
 func main() {
-	var wg sync.WaitGroup
 	// Parse second console arg for get number of workers
 	if len(os.Args) < 2 {
 		log.Fatal("Enter number of workers after program name.")
@@ -50,16 +47,14 @@ func main() {
 	// Create channel for workers
 	channel := make(chan any, workersNumberAsInt)
 	// Create entered number of workers
-	wg.Add(workersNumberAsInt)
 	for i := 0; i < workersNumberAsInt; i++ {
-		go worker(&wg, channel, i)
+		go worker(channel, i)
 	}
 	// Endlessly sending data to channel and wating interrupt
 	for {
 		select {
 		case <-cancelChannel:
 			close(channel)
-			wg.Wait()
 			return
 		default:
 			channel <- time.Now().UnixNano()
